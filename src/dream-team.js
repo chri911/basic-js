a99a67c5f5d3e2227cf9f05b29d0b459d460f0d9
chri911 <delightsome@bigmir.net>
2020-09-25 18:54:25 +0300

    feat: complete 3 tasks

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false;
+  };
+  name = '';
+  members.sort();
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+    let clean = members[i].trim().toUpperCase();
+        name += clean[0];
+    }
+  }
+  return name.split('').sort().join('');
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options ) {
+
+if( options.hasOwnProperty('separator')){
+  if (options.hasOwnProperty('addition') ){
+    if (options.hasOwnProperty('additionSeparator')) {
+      return (String(str) + options.addition + (options.additionSeparator + options.addition).repeat(options.additionRepeatTimes - 1) + options.separator).repeat(options.repeatTimes - 1) + (String(str) + options.addition + (options.additionSeparator + options.addition).repeat(options.additionRepeatTimes - 1));
+    } else {
+      return (String(str) + options.addition + ("|" + options.addition).repeat(options.additionRepeatTimes - 1) + options.separator).repeat(options.repeatTimes - 1) + (String(str) + options.addition + ("|" + options.addition).repeat(options.additionRepeatTimes - 1));
+    }
+  }  else {
+      return (str + options.separator).repeat(options.repeatTimes - 1) + str;
+  }
+} else{
+  return (str + "+").repeat(options.repeatTimes - 1) + str;
+};
 };
   
